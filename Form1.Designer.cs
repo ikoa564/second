83294265215444b9bb24e511f17a1a5d0fb96e74
Abdeev Ilnur <ilnur3038@yandex.ru>
2024-09-13 23:53:00 +0500

    Изменен цвет фона на оранжевый + увеличено окно

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -34,7 +34,7 @@
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Times New Roman", 14F);
-            this.button1.Location = new System.Drawing.Point(125, 107);
+            this.button1.Location = new System.Drawing.Point(285, 195);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(125, 51);
             this.button1.TabIndex = 0;
@@ -46,8 +46,8 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.BackColor = System.Drawing.Color.Gold;
-            this.ClientSize = new System.Drawing.Size(387, 288);
+            this.BackColor = System.Drawing.Color.Orange;
+            this.ClientSize = new System.Drawing.Size(718, 411);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Блокнот";
